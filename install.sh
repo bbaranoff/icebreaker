7a35737008ea2b0e44ecf80947fe7c39f46710ac
bbaranoff <bastienbaranoff@hotmail.fr>
2022-08-15 15:58:32 +0000

    alpha_working

--- install.sh
+++ install.sh
@@ -1,48 +1,66 @@
 #!/bin/bash
-apt install gcc-9 g++-9 gcc-10 g++-10 git -y
+cd /home/ubuntu/icebreaker
+rm -rf libosmo*
+apt install libdbd-sqlite3 gcc-9 g++-9 gcc-10 g++-10 git autoconf pkg-config libtool build-essential libtalloc-dev libpcsclite-dev gnutls-dev python2 python2-dev fftw3-dev libsctp-dev libdbi-dev -y
+cp /usr/bin/python2 /usr/bin/python
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 90 --slave /usr/bin/g++ g++ /usr/bin/g++-9
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100 --slave /usr/bin/g++ g++ /usr/bin/g++-10
+update-alternatives --set gcc /usr/bin/gcc-9
 git clone git://git.osmocom.org/libosmocore.git
-cd libosmocore
+cd  libosmocore
 git checkout 1.1.0
-autoreconf -i
+autoreconf -fi
 ./configure
 make
 make install
 ldconfig
-cd /opt/IMSI_Catcher
+cd ..
 git clone git://git.osmocom.org/libosmo-dsp.git
 cd libosmo-dsp
-autoreconf -i
+libtoolize && autoreconf -fi
+autoreconf -fi
 ./configure
 make
 make install
-update-alternatives --set gcc /usr/bin/gcc-9
+ldconfig
 apt install -y libortp-dev
-cd /opt/IMSI_Catcher
+cd ..
+
+git clone https://github.com/osmocom/osmocom-bb
+cd osmocom-bb/src
+git checkout fixeria/trxcon
+make nofirmware
+
+cd ../..
 git clone https://github.com/osmocom/libosmo-abis
-cd /opt/IMSI_Catcher/libosmo-abis
+cd libosmo-abis
 git checkout 0.8.1
 autoreconf -fi && ./configure --disable-dahdi && make -j4 && make install && ldconfig
 
-cd /opt/IMSI_Catcher
+cd ..
 git clone https://github.com/osmocom/libosmo-netif
-cd /opt/IMSI_Catcher/libosmo-netif
+cd libosmo-netif
 git checkout 0.6.0
 autoreconf -fi && ./configure && make -j4 && make install && ldconfig
 
-cd /home/ubuntu/icebreaker/bsc-2rfa
+cd ../bsc-2rfa/openbsc
 autoreconf -fi && ./configure && make -j4 && make install && ldconfig
 
-cd /opt/IMSI_Catcher
+cd ../..
 git clone https://github.com/osmocom/osmo-bts
-cd /opt/IMSI_Catcher/osmo-bts
+cd osmo-bts
 git checkout 0.8.1
 autoreconf -fi && ./configure --enable-trx && make -j4 && make install && ldconfig
 
-git clone git://git.osmocom.org/libosmocore.git
-cd libosmocore
+cd ../libosmocore
 git checkout 0.9.0
 make
 make install
 ldconfig
+
+cd ../bb-2rfa/src
+make nofirmware
+cd ../../kc_debug/src
+make nofirmware
+
+
